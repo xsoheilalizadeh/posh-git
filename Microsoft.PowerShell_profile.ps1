b5032090845a607d008d73a43538a0e3b73316cd
dev.soheilalizadeh <dev.soheilalizadeh@gmail.com>
2018-12-20 16:34:40 +0330

    Add powershell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,26 @@
+Import-Module posh-git
+
+<#
+ # Remove Default Path 
+ #>
+$GitPromptSettings.DefaultPromptPath = ''
+
+<#
+ # Change the posh-git text symbols
+ #>
+$GitPromptSettings.BeforeText = '('
+$GitPromptSettings.AfterText  = ')'
+
+<#
+ # Overrride the default powershell prompt 
+ #>
+function prompt {
+    
+  $p = Split-Path -leaf -path (Get-Location)
+  
+    " $p" + ":"; 
+  
+    & $GitPromptScriptBlock
+}
+
+  
